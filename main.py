c58dde5681acff65716839c8dd800cc426671f09
learnerliujiahui <liujiahui96220@126.com>
2019-03-15 00:37:45 +0800

    message

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ import argparse
 import time
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import models
 from models import densenet, condensenet
-from utils import AverageMeter
+from utils import AverageMeter, read_train_data, adjust_learning_rate, load_checkpoint, accuracy
 
 
 # version pytorch 1.0
@@ -66,8 +67,13 @@ def main():
 
     # set model
     model = getattr(models, args.model)(args)
-    print(model)
 
+    if torch.cuda.device_count():
+        model = torch.nn.DataParallel(model)  # for multi-GPU training
+    if torch.cuda.is_avilable():
+        model.cuda()
+
+    print(model)
 
     if args.mode == 'train':
         # get the training loader and validation loader
@@ -79,7 +85,8 @@ def main():
         else:
             start_epoch = args.start_epoch
 
-        train(start_epoch=start_epoch, epochs=args.epochs, model=model, train_set=train_set, val_set=val_set)
+        train(startepoch=start_epoch, epochs=args.epochs, model=model, train_set=train_set,
+              val_set=val_set, resume=args.resume)
 
     elif args.mode =='test':
         test(model=model, )
@@ -87,14 +94,38 @@ def main():
         raise NotImplementedError
 
 
-
-def train(start_epoch, epochs, model, train_set, val_set):
+def train(startepoch, epochs, model, train_set, val_set, resume):
     train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
 
     val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=args.workers, pin_memory=True)
 
+    # set the loss function and optimizer(SGD)
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum,
+                                weight_decay=args.weight_decay, nesterov=True)
+
+    # recover the parameters from checkpoint files
+    if resume:
+        checkpoint = load_checkpoint(args)
+        if checkpoint is not None:
+            start_epoch = checkpoint['epoch'] + 1
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+    else:
+        start_epoch = startepoch
+
+    # starting training
     for epoch in range(start_epoch, epochs):
-        learned_module_list = []
+        batch_time = AverageMeter()
+        data_time = AverageMeter()
+        losses = AverageMeter()
+        top1 = AverageMeter()
+        top5 = AverageMeter()
+        learned_module_list = []  # add all the LGC layers into the list
+
+        # Switch to train mode
+        model.train()
         # Find all learned convs to prepare for group lasso loss
         for m in model.modules():
             if m.__str__().startswith('LearnedGroupConv'):
@@ -102,75 +133,65 @@ def train(start_epoch, epochs, model, train_set, val_set):
 
         running_lr = None
 
+        beginTime = time.time()
         for i, (input, target) in enumerate(train_loader):
+            progress = float(epoch * len(train_loader) + i) / (epochs * len(train_loader))
+            args.progress = progress
+
+            lr = adjust_learning_rate(optimizer, epoch, args, batch=i, nBatch=len(train_loader), method=args.lr_type)
+            if running_lr is None:
+                running_lr = lr
+
+            data_time.update(time.time() - end)
+
+            # input Tensor CPU --> GPU
             if torch.cuda.is_avilable():
                 input = input.cuda()
                 target = target.cuda()
 
-            input = Variable(input, requires_grad=True)
-            target = Variable(target)
-            loss = None
-            output, featureMaps = model(input, target)
+            # Tensor --> Variable --> model
+            inputVar = Variable(input, requires_grad=True)
+            targetVar = Variable(target)
+
+            # compute the result
+            output, featureMaps = model(inputVar, progress)
+            loss = criterion(output, targetVar)
+
+            # Add group lasso loss to the basic loss value
+            if args.group_lasso_lambda > 0:
+                lasso_loss = 0
+                for m in learned_module_list:
+                    lasso_loss = lasso_loss + m.lasso_loss
+                loss = loss + args.group_lasso_lambda * lasso_loss
+
+            # Measure accuracy and record loss
+            prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+            losses.update(loss.data[0], input.size(0))
+            top1.update(prec1[0], input.size(0))
+            top5.update(prec5[0], input.size(0))
+
+            # Compute gradient and do SGD step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            batch_time.update(time.time() - beginTime)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                      'Time {batch_time.val:.3f}\t'  # ({batch_time.avg:.3f}) '
+                      'Data {data_time.val:.3f}\t'  # ({data_time.avg:.3f}) '
+                      'Loss {loss.val:.4f}\t'  # ({loss.avg:.4f}) '
+                      'Prec@1 {top1.val:.3f}\t'  # ({top1.avg:.3f}) '
+                      'Prec@5 {top5.val:.3f}\t'  # ({top5.avg:.3f})'
+                      'lr {lr: .4f}'.format(epoch, i, len(train_loader), batch_time=batch_time,
+                                            data_time=data_time, loss=losses, top1=top1, top5=top5, lr=lr))
 
 
 def test():
     pass
 
 
-def read_train_data(datadir, data, mode):
-    data_dir = os.path.join(datadir, data)
-    if data == 'cifar10':
-
-        num_classes = 10
-        image_size = 32
-        mean=[0.4914, 0.4824, 0.4467]
-        std=[0.2471, 0.2435, 0.2616]
-        train_set = datasets.CIFAR10(data_dir, train=True, download=True, transform=transforms.Compose([
-                                                                                    transforms.RandomCrop(image_size, padding=4),
-                                                                                    transforms.RandomHorizontalFlip(),
-                                                                                    transforms.ToTensor(),
-                                                                                    transforms.Normalize(mean=mean, std=std)]))
-        val_set = datasets.CIFAR10(data_dir, train=False, transform=transforms.Compose([
-                                                                                    transforms.ToTensor(),
-                                                                                    transforms.Normalize(mean=mean, std=std)]))
-    elif data == 'cifar100':
-        num_classes = 100
-        image_size = 32
-        mean=[0.5071, 0.4867, 0.4408]
-        std=[0.2675, 0.2565, 0.2761]
-        train_set = datasets.CIFAR100(data_dir, train=True, download=True, transform=transforms.Compose([
-                                                                              transforms.RandomCrop(image_size, padding=4),
-                                                                              transforms.RandomHorizontalFlip(),
-                                                                              transforms.ToTensor(),
-                                                                              transforms.Normalize(mean=mean, std=std)]))
-        val_set = datasets.CIFAR100(data_dir, train=False, transform=transforms.Compose([
-                                                                            transforms.ToTensor(),
-                                                                            transforms.Normalize(mean=mean, std=std)]))
-    elif data == 'imagenet':
-        num_classes = 1000
-        image_size = 224
-        traindir = os.path.join(data_dir, 'train')
-        valdir = os.path.join(data_dir, 'val')
-        mean=[0.485, 0.456, 0.406]
-        std=[0.229, 0.224, 0.225]
-        train_set = datasets.ImageFolder(traindir, transforms.Compose([transforms.RandomSizedCrop(image_size),
-                                                                       transforms.RandomHorizontalFlip(),
-                                                                       transforms.ToTensor(),
-                                                                       transforms.Normalize(mean=mean, std=std)]))
-
-        val_set = datasets.ImageFolder(valdir, transforms.Compose([transforms.Scale(256),
-                                                                   transforms.CenterCrop(224),
-                                                                   transforms.ToTensor(),
-                                                                   transforms.Normalize(mean=mean, std=std)]))
-
-    else:
-        raise NotImplementedError
-
-    return train_set, val_set
-
-
-
-
 if __name__ =='__main__':
-    print('begin')
     main()
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-
+import os
+import math
+import torch
+from torchvision import datasets, transforms
 
 class AverageMeter(object):
     """
@@ -19,3 +22,115 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+def read_train_data(datadir, data, mode):
+    data_dir = os.path.join(datadir, data)
+    if data == 'cifar10':
+
+        num_classes = 10
+        image_size = 32
+        mean=[0.4914, 0.4824, 0.4467]
+        std=[0.2471, 0.2435, 0.2616]
+        train_set = datasets.CIFAR10(data_dir, train=True, download=True, transform=transforms.Compose([
+                                                                                    transforms.RandomCrop(image_size, padding=4),
+                                                                                    transforms.RandomHorizontalFlip(),
+                                                                                    transforms.ToTensor(),
+                                                                                    transforms.Normalize(mean=mean, std=std)]))
+        val_set = datasets.CIFAR10(data_dir, train=False, transform=transforms.Compose([
+                                                                                    transforms.ToTensor(),
+                                                                                    transforms.Normalize(mean=mean, std=std)]))
+    elif data == 'cifar100':
+        num_classes = 100
+        image_size = 32
+        mean=[0.5071, 0.4867, 0.4408]
+        std=[0.2675, 0.2565, 0.2761]
+        train_set = datasets.CIFAR100(data_dir, train=True, download=True, transform=transforms.Compose([
+                                                                              transforms.RandomCrop(image_size, padding=4),
+                                                                              transforms.RandomHorizontalFlip(),
+                                                                              transforms.ToTensor(),
+                                                                              transforms.Normalize(mean=mean, std=std)]))
+        val_set = datasets.CIFAR100(data_dir, train=False, transform=transforms.Compose([
+                                                                            transforms.ToTensor(),
+                                                                            transforms.Normalize(mean=mean, std=std)]))
+    elif data == 'imagenet':
+        num_classes = 1000
+        image_size = 224
+        traindir = os.path.join(data_dir, 'train')
+        valdir = os.path.join(data_dir, 'val')
+        mean=[0.485, 0.456, 0.406]
+        std=[0.229, 0.224, 0.225]
+        train_set = datasets.ImageFolder(traindir, transforms.Compose([transforms.RandomSizedCrop(image_size),
+                                                                       transforms.RandomHorizontalFlip(),
+                                                                       transforms.ToTensor(),
+                                                                       transforms.Normalize(mean=mean, std=std)]))
+
+        val_set = datasets.ImageFolder(valdir, transforms.Compose([transforms.Scale(256),
+                                                                   transforms.CenterCrop(224),
+                                                                   transforms.ToTensor(),
+                                                                   transforms.Normalize(mean=mean, std=std)]))
+
+    else:
+        raise NotImplementedError
+
+    return train_set, val_set
+
+
+def adjust_learning_rate(optimizer, epoch, args, batch=None, nBatch=None, method='cosine'):
+    """
+
+    :param optimizer:
+    :param epoch:
+    :param args:
+    :param batch:
+    :param nBatch:
+    :param method:
+    :return:
+    """
+    if method == 'cosine':
+        T_total = args.epochs * nBatch
+        T_cur = (epoch % args.epochs) * nBatch + batch
+        lr = 0.5 * args.lr * (1 + math.cos(math.pi * T_cur / T_total))
+    elif method == 'multistep':
+        if args.data in ['cifar10', 'cifar100']:
+            lr, decay_rate = args.lr, 0.1
+            if epoch >= args.epochs * 0.75:
+                lr *= decay_rate**2
+            elif epoch >= args.epochs * 0.5:
+                lr *= decay_rate
+        else:
+            """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+            lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return lr
+
+
+def load_checkpoint(args):
+    model_dir = os.path.join(args.savedir, 'save_models')
+    latest_filename = os.path.join(model_dir, 'latest.txt')
+    if os.path.exists(latest_filename):
+        with open(latest_filename, 'r') as fin:
+            model_filename = fin.readlines()[0]
+    else:
+        return None
+    print("=> loading checkpoint '{}'".format(model_filename))
+    state = torch.load(model_filename)
+    print("=> loaded checkpoint '{}'".format(model_filename))
+    return state
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
