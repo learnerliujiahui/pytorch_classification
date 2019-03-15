179093f1e1a352e03f35aa48e40ce7f445e6dabb
liujiahui <liujiahui96220@126.com>
2019-03-15 20:11:56 +0800

    message

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import models
 from models import densenet, condensenet
-from utils import AverageMeter, read_train_data, adjust_learning_rate, load_checkpoint, accuracy
+from utils import AverageMeter, read_train_data, adjust_learning_rate, load_checkpoint, accuracy, measure_model
 
 
 # version pytorch 1.0
@@ -28,9 +28,12 @@ parser.add_argument('--mode', default='train', choices=['train', 'test'])  # tra
 parser.add_argument('--featureVisualize', default=True)
 parser.add_argument('--data', default='cifar10', choices=['cifar10', 'cifar100', 'imagenet'])
 parser.add_argument('--data_dir', default='/home/liujiahui/data_zoo/')  # + cifar10/cifar100/...
-parser.add_argument('--save_dir', default='/home/liujiahui/PycharmProjects/efficient_densenet_pytorch/log0310')
+parser.add_argument('--save_dir', default='/home/liujiahui/PycharmProjects/efficient_densenet_pytorch/log0315')
 parser.add_argument('--para_dir', default='/home/liujiahui/PycharmProjects/efficient_densenet_pytorch/log0309')
-parser.add_argument('--model', default='denseNet')  # DensrNet/ CondenseNet/ RsNet
+parser.add_argument('--gpu', default='0')
+parser.add_argument('--workers', default=8, type=int, metavar='N', help='number of data loading workers (default: 4)')
+parser.add_argument('--print-freq', default=10, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--model', default='condensenet')  # DensrNet/ CondenseNet/ RsNet
 parser.add_argument('--test_size', default=500)
 parser.add_argument('--start_epoch', default=0)
 parser.add_argument('--epochs', default=300)
@@ -40,15 +43,18 @@ parser.add_argument('--batch_size',default=256)
 # model hyper-parameters:
 parser.add_argument('--lr', default=0.1, help='the intial learning rate value')
 parser.add_argument('--lr_type', default='cosine', choices=['cosine', 'multistep'])
-
 parser.add_argument('--weight_decay', default=1e-4)
 parser.add_argument('--momentum', default=0.9)
+parser.add_argument('--num_classes', default=10)
 # for densenet
 parser.add_argument('--depth', default=100)
 parser.add_argument('--growth_rate', default=12)
 # for condensenet
-parser.add_argument('--stage', default='14-14-14')
+parser.add_argument('--stages', default='14-14-14')
 parser.add_argument('--growth', default='8-16-32')
+parser.add_argument('--reduction', default=0.5, type=float, metavar='R', help='transition reduction (default: 0.5)')
+parser.add_argument('--dropout-rate', default=0, type=float, help='drop out (default: 0)')
+parser.add_argument('--group-lasso-lambda', default=0., type=float, metavar='LASSO', help='group lasso loss weight (default: 0)')
 parser.add_argument('--bottleneck', default=4)
 parser.add_argument('--group-1x1', default=4, help='1x1 group convolution (default: 4)')
 parser.add_argument('--group-3x3', default=4, help='3x3 group convolution (default: 4)')
@@ -56,21 +62,37 @@ parser.add_argument('--condense-factor', default=4, help='condense factor (defau
 
 args = parser.parse_args()
 
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu  # set the GPU number
+args.stages = list(map(int, args.stages.split('-')))
+args.growth = list(map(int, args.growth.split('-')))
+
+if args.condense_factor is None:
+    args.condense_factor = args.group_1x1
+
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu  # set the GPU number
-    block_config = list(map(int, args.stages.split('-')))
-    block_growth = list(map(int, args.growth.split('-')))
-
-    if args.condense_factor is None:
-        args.condense_factor = args.group_1x1
 
     # set model
     model = getattr(models, args.model)(args)
 
+    if args.data == 'cifar10':
+        image_size = 32
+        args.num_classes = 10
+    elif args.data == 'cifar100':
+        image_size = 32
+        args.num_classes = 100
+    elif args.data == 'imagenet':
+        image_size = 224
+        args.num_classes = 1000
+    else:
+        raise NotImplementedError
+
+    n_flops, n_params = measure_model(model, image_size, image_size)
+    print('FLOPs: %.2fM, Params: %.2fM' % (n_flops / 1e6, n_params / 1e6))
+
     if torch.cuda.device_count():
         model = torch.nn.DataParallel(model)  # for multi-GPU training
-    if torch.cuda.is_avilable():
+    if torch.cuda.is_available():
         model.cuda()
 
     print(model)
@@ -78,7 +100,7 @@ def main():
     if args.mode == 'train':
         # get the training loader and validation loader
         train_set, val_set = read_train_data(datadir=args.data_dir, data=args.data, mode='train')
-
+        test_set = read_test_set()
         # set the start epoch value
         if args.resume:
             start_epoch = None
@@ -101,8 +123,7 @@ def train(startepoch, epochs, model, train_set, val_set, resume):
 
     # set the loss function and optimizer(SGD)
     criterion = nn.CrossEntropyLoss().cuda()
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum,
-                                weight_decay=args.weight_decay, nesterov=True)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
 
     # recover the parameters from checkpoint files
     if resume:
@@ -142,10 +163,10 @@ def train(startepoch, epochs, model, train_set, val_set, resume):
             if running_lr is None:
                 running_lr = lr
 
-            data_time.update(time.time() - end)
+            data_time.update(time.time() - beginTime)
 
             # input Tensor CPU --> GPU
-            if torch.cuda.is_avilable():
+            if torch.cuda.is_available():
                 input = input.cuda()
                 target = target.cuda()
 
@@ -189,8 +210,50 @@ def train(startepoch, epochs, model, train_set, val_set, resume):
                                             data_time=data_time, loss=losses, top1=top1, top5=top5, lr=lr))
 
 
-def test():
-    pass
+def test(model, val_loader):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # Switch to evaluate mode
+    model.eval()
+
+    begin_time= time.time()
+
+    for i, (input, target) in enumerate(val_loader):
+        if torch.cuda.is_available():
+            input = input.cuda()
+            target = target.cuda(async=True)
+
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+
+        criterion = nn.CrossEntropyLoss().cuda()
+
+        # Compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # Measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # Measure elapsed time
+        batch_time.update(time.time() - begin_time)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(i, len(val_loader), batch_time=batch_time,
+                                                                  loss=losses, top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
 
 
 if __name__ =='__main__':
